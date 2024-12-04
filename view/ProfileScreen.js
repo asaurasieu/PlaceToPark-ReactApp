41029b88a2465ac72d3a9d70d46de1782bebd0f3
asaurasieu <asauras.ieu2021@student.ie.edu>
2024-12-04 19:46:23 +0100

    fetching location and parking areas with Google Distance Matrix and fixing errors

--- view/ProfileScreen.js
+++ view/ProfileScreen.js
@@ -1,162 +1,172 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
-import { GestureHandlerRootView, RectButton } from 'react-native-gesture-handler';
-import { useData } from '../common/userContext';
+import {View, Text, Image, StyleSheet, ScrollView} from 'react-native';
+import {GestureHandlerRootView, RectButton} from 'react-native-gesture-handler';
+import {useData} from '../common/userContext';
 
+const ProfileScreen = ({navigation, route}) => {
+  const {userData} = useData();
 
-const ProfileScreen = ({ navigation, route }) => {
-    const { userData } = useData();
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}>
+        <Image
+          source={require('../assets/photo5.jpg')}
+          style={styles.coverImage}
+        />
+        <View style={styles.profileSection}>
+          <Image
+            source={require('../assets/profile.jpg')}
+            style={styles.profileImage}
+          />
+          <Text style={styles.name}>
+            {userData.name || 'No Name Available'}
+          </Text>
+          <Text style={styles.profession}>
+            {userData.profession || 'Profession not set'}
+          </Text>
+          <Text style={styles.location}>
+            {userData.location || 'Location not set'}
+          </Text>
+          <Text style={styles.location}>
+            {userData.dateOfBirth || 'Date of birth not set'}
+          </Text>
+          <View style={styles.statsContainer}>
+            <View style={styles.statItem}>
+              <Text style={styles.statNumber}>30</Text>
+              <Text style={styles.statLabel}>Active days</Text>
+            </View>
+            <View style={[styles.statItem, {marginLeft: -30}]}>
+              <Text style={styles.statNumber}>20</Text>
+              <Text style={styles.statLabel}>Friends</Text>
+            </View>
+          </View>
+          <View style={styles.buttonContainer}>
+            <RectButton
+              style={styles.button}
+              onPress={() => navigation.navigate('EditProfile')}>
+              <Text style={styles.buttonText}>Edit Profile</Text>
+            </RectButton>
+            <RectButton style={[styles.button, styles.buttonOutline]}>
+              <Text style={[styles.buttonText, styles.buttonOutlineText]}>
+                Add Friend
+              </Text>
+            </RectButton>
+          </View>
 
-    return (
-        <GestureHandlerRootView style={styles.container}>
-            <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-                <Image
-                    source={require('../assets/photo5.jpg')}
-                    style={styles.coverImage}
-                />
-                <View style={styles.profileSection}>
-                    <Image
-                        source={require('../assets/profile.jpg')}
-                        style={styles.profileImage}
-                    />
-                    <Text style={styles.name}>{userData.name || 'No Name Available'}</Text>
-                    <Text style={styles.profession}>{userData.profession || 'Profession not set'}</Text>
-                    <Text style={styles.location}>{userData.location || 'Location not set'}</Text>
-                    <Text style={styles.location}>{userData.dateOfBirth || 'Date of birth not set'}</Text>
-                    <View style={styles.statsContainer}>
-                        <View style={styles.statItem}>
-                            <Text style={styles.statNumber}>30</Text>
-                            <Text style={styles.statLabel}>Active days</Text>
-                        </View>
-                        <View style={[styles.statItem, { marginLeft: -30 }]}>
-                            <Text style={styles.statNumber}>20</Text>
-                            <Text style={styles.statLabel}>Friends</Text>
-                        </View>
-                    </View>
-                    <View style={styles.buttonContainer}>
-                        <RectButton
-                            style={styles.button}
-                            onPress={() => navigation.navigate('EditProfile')}
-                        >
-                            <Text style={styles.buttonText}>Edit Profile</Text>
-                        </RectButton>
-                        <RectButton style={[styles.button, styles.buttonOutline]}>
-                            <Text style={[styles.buttonText, styles.buttonOutlineText]}>Add Friend</Text>
-                        </RectButton>
-                    </View>
-
-                    {/* Recently Visited Section */}
-                    <View style={styles.recentlyVisitedSection}>
-                        <Text style={styles.recentlyVisitedTitle}>Recently Visited</Text>
-                        {userData?.lastSearch && userData.lastSearch.length > 0 ? (
-                            userData.lastSearch.map((location, index) => (
-                                <Text key={index} style={styles.recentlyVisitedText}>
-                                    {location}
-                                </Text>
-                            ))
-                        ) : (
-                            <Text style={styles.recentlyVisitedText}>No recent searches</Text>
-                        )}
-                    </View>
-                </View>
-            </ScrollView>
-        </GestureHandlerRootView>
-    );
+          {/* Recently Visited Section */}
+          <View style={styles.recentlyVisitedSection}>
+            <Text style={styles.recentlyVisitedTitle}>Recently Visited</Text>
+            {userData?.lastSearch && userData.lastSearch.length > 0 ? (
+              userData.lastSearch.map((location, index) => (
+                <Text key={index} style={styles.recentlyVisitedText}>
+                  {location}
+                </Text>
+              ))
+            ) : (
+              <Text style={styles.recentlyVisitedText}>No recent searches</Text>
+            )}
+          </View>
+        </View>
+      </ScrollView>
+    </GestureHandlerRootView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    contentContainer: {
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-    },
-    coverImage: {
-        width: '100%',
-        height: 200,
-    },
-    profileSection: {
-        alignItems: 'center',
-        marginTop: -50,
-        width: '100%',
-    },
-    profileImage: {
-        width: 100,
-        height: 100,
-        borderRadius: 50,
-        borderWidth: 4,
-        borderColor: 'white',
-    },
-    name: {
-        fontSize: 24,
-        fontWeight: 'bold',
-    },
-    profession: {
-        fontSize: 16,
-        color: 'gray',
-    },
-    location: {
-        fontSize: 16,
-        color: 'gray',
-    },
-    statsContainer: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        width: '100%',
-        marginTop: 20,
-    },
-    statItem: {
-        alignItems: 'center',
-        marginHorizontal: 120,
-    },
-    statNumber: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    statLabel: {
-        fontSize: 14,
-        color: 'gray',
-    },
-    buttonContainer: {
-        flexDirection: 'row',
-        marginTop: 20,
-    },
-    button: {
-        backgroundColor: '#007bff',
-        padding: 10,
-        borderRadius: 5,
-        marginHorizontal: 21,
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 18,
-    },
-    buttonOutline: {
-        backgroundColor: 'transparent',
-        borderWidth: 1,
-        borderColor: '#007bff',
-    },
-    buttonOutlineText: {
-        color: '#007bff',
-    },
-    recentlyVisitedSection: {
-        width: '100%',
-        marginTop: 30,
-    },
-    recentlyVisitedTitle: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: 'black',
-        marginBottom: 10,
-        marginLeft: 10,
-    },
-    recentlyVisitedText: {
-        fontSize: 16,
-        color: 'gray',
-        marginLeft: 10,
-        marginBottom: 5,
-    },
+  container: {
+    flex: 1,
+  },
+  contentContainer: {
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  coverImage: {
+    width: '100%',
+    height: 200,
+  },
+  profileSection: {
+    alignItems: 'center',
+    marginTop: -50,
+    width: '100%',
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderWidth: 4,
+    borderColor: 'white',
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  profession: {
+    fontSize: 16,
+    color: 'gray',
+  },
+  location: {
+    fontSize: 16,
+    color: 'gray',
+  },
+  statsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
+    marginTop: 20,
+  },
+  statItem: {
+    alignItems: 'center',
+    marginHorizontal: 120,
+  },
+  statNumber: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  statLabel: {
+    fontSize: 14,
+    color: 'gray',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: '#007bff',
+    padding: 10,
+    borderRadius: 5,
+    marginHorizontal: 21,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+  },
+  buttonOutline: {
+    backgroundColor: 'transparent',
+    borderWidth: 1,
+    borderColor: '#007bff',
+  },
+  buttonOutlineText: {
+    color: '#007bff',
+  },
+  recentlyVisitedSection: {
+    width: '100%',
+    marginTop: 30,
+  },
+  recentlyVisitedTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'black',
+    marginBottom: 10,
+    marginLeft: 10,
+  },
+  recentlyVisitedText: {
+    fontSize: 16,
+    color: 'gray',
+    marginLeft: 10,
+    marginBottom: 5,
+  },
 });
 
 export default ProfileScreen;
--- view/SearchScreen.js
+++ view/SearchScreen.js
@@ -1,159 +1,210 @@
-import React, { useState } from 'react';
-import { View, Image, Text, StyleSheet, Alert } from 'react-native';
-import { GestureHandlerRootView, RectButton } from 'react-native-gesture-handler';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import { REACT_APP_GOOGLE_API_KEY } from '@env'; // Ensure your `.env` is configured properly
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Alert, FlatList} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
+import {REACT_APP_GOOGLE_API_KEY} from '@env'; // Ensure your `.env` is configured properly
+import {useData} from '../common/userContext';
+import {db} from '../common/firebase';
 
-import { useData } from '../common/userContext';
-import { db } from '../common/firebase';
+const SearchScreen = ({navigation}) => {
+  const {userData, setUserData, email} = useData();
+  const [nearestAreas, setNearestAreas] = useState([]);
+  const [isFocused, setIsFocused] = useState(false);
 
-const localImage = require('../assets/ciervos.jpg');
+  const handleSearchSelection = async (location, description) => {
+    console.log(location);
 
-const SearchScreen = ({ navigation }) => {
-    const { userData, setUserData, email } = useData();
-    const [locationSearch, setLocationSearch] = useState('');
-    const [showImage, setShowImage] = useState(false);
-    const [isFocused, setIsFocused] = useState(false);
+    // Update the user's lastSearch array
+    const updatedLastSearch = [
+      description,
+      ...(userData.lastSearch || []),
+    ].slice(0, 4);
 
-    const locationData = {
-        address: locationSearch,
-        imageUri: localImage,
+    const updatedUserData = {
+      ...userData,
+      lastSearch: updatedLastSearch,
     };
 
-    const handleSearchSelection = (location) => {
-        setLocationSearch(location);
+    // Save the updated search history to the database
+    try {
+      await db
+        .collection('users')
+        .doc(email)
+        .set(updatedUserData, {merge: true});
+      setUserData(updatedUserData);
+    } catch (error) {
+      Alert.alert('Error', 'Failed to save search: ' + error.message);
+    }
 
-        // Update the user's lastSearch array
-        const updatedLastSearch = [location, ...(userData.lastSearch || [])].slice(0, 4);
+    // Fetch parking data and calculate distances
+    try {
+      const parkingAreas = await fetchParkingAreas();
+      const origin = `${location.lat},${location.lng}`;
+      const distances = await fetchDistances(origin, parkingAreas);
+      console.log(distances);
 
-        const updatedUserData = {
-            ...userData,
-            lastSearch: updatedLastSearch,
-        };
+      const nearest = distances
+        .sort((a, b) => a.distance - b.distance)
+        .slice(0, 3);
+      nearest.unshift({area: {barrio: "04-01 RECOLETOS", bateria_linea: "Batería", calle: "CASTELLO, CALLE, DE", "color": "077214010 Verde", "distrito": "04  SALAMANCA", "id": "1001", "latitude": 40.428568305, "longitude": -3.681224762, "num_finca": 63, "num_plazas": 21})
+      setNearestAreas(nearest);
+    } catch (error) {
+      Alert.alert('Error', 'Failed to calculate distances: ' + error.message);
+    }
+  };
 
-        // Save the updated search history to the database
-        db.collection('users')
-            .doc(email)
-            .set(updatedUserData, { merge: true })
-            .then(() => {
-                setUserData(updatedUserData);
-            })
-            .catch((error) => {
-                Alert.alert('Error', 'Failed to save search: ' + error.message);
-            });
-    };
+  const fetchParkingAreas = async () => {
+    try {
+      const snapshot = await db.collection('parking_areas').get();
+      return snapshot.docs.map(doc => ({id: doc.id, ...doc.data()}));
+    } catch (error) {
+      Alert.alert('Error', 'Failed to fetch parking areas: ' + error.message);
+      return [];
+    }
+  };
 
-    const onImagePress = () => {
-        if (!locationSearch) {
-            Alert.alert('Error', 'Please select a location first.');
-            return;
-        }
+  const fetchDistances = async (origin, destinations) => {
+    const destinationString = destinations
+      .slice(0, 10)
+      .map(loc => `${loc.latitude},${loc.longitude}`)
+      .join('|');
 
-        navigation.navigate('SlotsScreen');
-    };
+    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${destinationString}&key=${REACT_APP_GOOGLE_API_KEY}`;
+    console.log(url);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
 
-    return (
-        <GestureHandlerRootView style={styles.container}>
-            <View style={styles.inputContainer}>
-                <GooglePlacesAutocomplete
-                    placeholder="Search for a location"
-                    onPress={(data, details = null) => {
-                        handleSearchSelection(data.description);
-                        setShowImage(true);
-                    }}
-                    query={{
-                        key: REACT_APP_GOOGLE_API_KEY,
-                        language: 'en',
-                    }}
-                    styles={{
-                        textInput: isFocused ? styles.textInputFocused : styles.textInput,
-                        container: { flex: 0 },
-                        listView: styles.listView,
-                    }}
-                    textInputProps={{
-                        onFocus: () => setIsFocused(true),
-                        onBlur: () => setIsFocused(false),
-                    }}
-                />
+      console.log(response);
+      if (data.status === 'OK') {
+        return data.rows[0].elements.map((el, index) => ({
+          distance: el.distance.value, // Distance in meters
+          area: destinations[index],
+        }));
+      } else {
+        Alert.alert(
+          'Error',
+          'Failed to fetch distances: ' + data.error_message,
+        );
+        return [];
+      }
+    } catch (error) {
+      Alert.alert(
+        'Error',
+        'Failed to connect to Google Distance Matrix: ' + error.message,
+      );
+      return [];
+    }
+  };
+
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <GooglePlacesAutocomplete
+          placeholder="Search for a location"
+          onPress={(data, details = null) => {
+            if (details && details.geometry) {
+              const location = details.geometry.location;
+              handleSearchSelection(location, data.description);
+            }
+          }}
+          query={{
+            key: REACT_APP_GOOGLE_API_KEY,
+            language: 'en',
+          }}
+          styles={{
+            textInput: isFocused ? styles.textInputFocused : styles.textInput,
+            container: {flex: 0},
+            listView: styles.listView,
+          }}
+          textInputProps={{
+            onFocus: () => setIsFocused(true),
+            onBlur: () => setIsFocused(false),
+          }}
+          fetchDetails={true}
+        />
+      </View>
+      {nearestAreas.length > 0 && (
+        <FlatList
+          data={nearestAreas}
+          keyExtractor={item => item.area.id}
+          renderItem={({item}) => (
+            <View style={styles.resultItem}>
+              <Text style={styles.resultText}>
+                {item.area.calle} ({item.area.distrito})
+              </Text>
+              <Text style={styles.resultText}>
+                Distance: {(item.distance / 1000).toFixed(2)} km
+              </Text>
+              <Text style={styles.resultText}>
+                Spaces: {item.area.num_plazas}
+              </Text>
             </View>
-            {showImage && (
-                <RectButton onPress={onImagePress} style={styles.imageContainer}>
-                    <Image
-                        source={locationData.imageUri}
-                        style={styles.image}
-                        onError={(e) => console.log('Error loading image:', e.nativeEvent.error)}
-                    />
-                    <Text style={styles.text}>{locationData.address}</Text>
-                </RectButton>
-            )}
-        </GestureHandlerRootView>
-    );
+          )}
+        />
+      )}
+    </GestureHandlerRootView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        paddingTop: 60,
-        alignItems: 'center',
-    },
-    inputContainer: {
-        width: '90%',
-        borderRadius: 25,
-        backgroundColor: '#f9f9f9',
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 4 },
-        shadowOpacity: 0.1,
-        shadowRadius: 10,
-        elevation: 4,
-        padding: 5,
-    },
-    textInput: {
-        height: 50,
-        borderRadius: 25,
-        backgroundColor: '#fff',
-        paddingLeft: 20,
-        fontSize: 16,
-        color: '#000',
-        borderWidth: 1,
-        borderColor: '#ddd',
-    },
-    textInputFocused: {
-        height: 50,
-        borderRadius: 25,
-        backgroundColor: '#fff',
-        paddingLeft: 20,
-        fontSize: 16,
-        color: '#000',
-        borderWidth: 1,
-        borderColor: '#007BFF',
-    },
-    listView: {
-        backgroundColor: '#fff',
-        borderRadius: 15,
-        marginHorizontal: 10,
-        elevation: 3,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2,
-        shadowRadius: 5,
-    },
-    imageContainer: {
-        marginTop: 20,
-        alignItems: 'center',
-        width: '90%',
-    },
-    image: {
-        width: '100%',
-        height: 200,
-        borderRadius: 15,
-    },
-    text: {
-        marginTop: 10,
-        fontSize: 16,
-        color: '#000',
-        textAlign: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 60,
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '90%',
+    borderRadius: 25,
+    backgroundColor: '#f9f9f9',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 4},
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 4,
+    padding: 5,
+  },
+  textInput: {
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#fff',
+    paddingLeft: 20,
+    fontSize: 16,
+    color: '#000',
+    borderWidth: 1,
+    borderColor: '#ddd',
+  },
+  textInputFocused: {
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#fff',
+    paddingLeft: 20,
+    fontSize: 16,
+    color: '#000',
+    borderWidth: 1,
+    borderColor: '#007BFF',
+  },
+  listView: {
+    backgroundColor: '#fff',
+    borderRadius: 15,
+    marginHorizontal: 10,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+  },
+  resultItem: {
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 10,
+  },
+  resultText: {
+    fontSize: 16,
+    color: '#333',
+  },
 });
 
 export default SearchScreen;
